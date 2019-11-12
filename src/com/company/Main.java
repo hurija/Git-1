99b9e0088b5356e6875bb4c1fbf5e163a2ee3513
hurija <paulina.a.ciesielska@gmail.com>
2019-11-12 19:42:12 +0100

    piersza zmiana w projekcie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+// sout -> System.out.println ();
+// CTRL + D -> duplikowanie lini kodu
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("Publikujemy pierwszy program!!!");
     }
 }
